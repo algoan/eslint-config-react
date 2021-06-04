e6269ee7b315c74d8ba4731887caf9d8374dd10e
Naji Astier <naji.astier@algoan.com>
2021-06-04 15:31:06 +0200

    refactor: move rules to index.js file + apply prettier

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-module.exports = require('./eslintrc.json');
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }],
+    'react/prop-types': 'off',
+    'no-console': 'warn',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
