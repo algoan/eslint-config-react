e5242bfb2150111c8489d307687e30e85367bfc4
Naji Astier <naji.astier@algoan.com>
2021-07-27 16:19:12 +0200

    feat: enforce naming conventions for everything

--- index.js
+++ index.js
@@ -20,6 +20,48 @@ module.exports = {
     'no-console': 'warn',
     'react/react-in-jsx-scope': 'off',
     'react/jsx-pascal-case': [2, { allowNamespace: true }],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: ['variableLike', 'memberLike', 'method'],
+        format: ['camelCase'],
+      },
+      {
+        selector: 'parameter',
+        // allow parameters with leading underscore
+        leadingUnderscore: 'allow',
+        format: ['camelCase'],
+      },
+      {
+        selector: ['function'],
+        format: ['camelCase', 'PascalCase'],
+      },
+      {
+        selector: 'property',
+        leadingUnderscore: 'allowSingleOrDouble',
+        format: ['camelCase'],
+      },
+      {
+        selector: 'objectLiteralProperty',
+        leadingUnderscore: 'allowSingleOrDouble',
+        // PascalCase for style object properties, camelCase for everything else
+        format: ['camelCase', 'PascalCase'],
+      },
+      {
+        selector: 'enumMember',
+        format: ['UPPER_CASE'],
+      },
+      {
+        selector: 'variable',
+        modifiers: ['const'],
+        // PascalCase for components, UPPER_CASE for constants, camelCase for everything else
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+      },
+    ],
   },
   settings: {
     react: {
